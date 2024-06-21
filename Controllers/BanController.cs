cd73ec0439275d03f92b3623359831f70f32578f
phamdat2004 <phamtiendat21072004@gmai.com>
2024-06-21 12:02:02 +0700

    update

---
+++ Controllers/BanController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Webbanhang.Controllers
+{
+    public class BanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult hienThi()
+        {
+            return View();
+        }
+    }
+}
